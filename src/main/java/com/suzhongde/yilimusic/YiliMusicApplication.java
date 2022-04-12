07bddb7b4bce426bdb897c000e11d662228f6651
suzhongde <2373544536@qq.com>
2022-04-13 05:28:53 +0800

    feat: add cross

--- src/main/java/com/suzhongde/yilimusic/YiliMusicApplication.java
+++ src/main/java/com/suzhongde/yilimusic/YiliMusicApplication.java
@@ -2,10 +2,8 @@ package com.suzhongde.yilimusic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
-@CrossOrigin
 public class YiliMusicApplication {
 
     public static void main(String[] args) {
